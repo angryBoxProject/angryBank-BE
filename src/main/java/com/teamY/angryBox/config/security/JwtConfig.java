a6336bd670f17c385be1cbe5ff2dc71e70436eb3
da77777 <84134297+da77777@users.noreply.github.com>
2022-04-09 17:46:39 +0900

    oauth, jwt 설정 중

---
+++ src/main/java/com/teamY/angryBox/config/security/JwtConfig.java
@@ -0,0 +1,15 @@
+package com.teamY.angryBox.config.security;
+
+import lombok.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JwtConfig {
+    //@Value("${jwt.secret}")
+    private String secret;
+
+//    @Bean
+//    public
+}
+
--- src/main/java/com/teamY/angryBox/config/security/SecurityConfig.java
+++ src/main/java/com/teamY/angryBox/config/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.teamY.angryBox.security;
+package com.teamY.angryBox.config.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
