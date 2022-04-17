d2cf844037bc33383d1dd20f606cb0eb196c292d
da77777 <84134297+da77777@users.noreply.github.com>
2022-04-17 18:06:55 +0900

    다이어리 작성 수정

--- src/main/java/com/teamY/angryBox/controller/DiaryController.java
+++ src/main/java/com/teamY/angryBox/controller/DiaryController.java
@@ -1,5 +1,7 @@
 package com.teamY.angryBox.controller;
 
+import com.teamY.angryBox.config.security.oauth.AuthTokenProvider;
+import com.teamY.angryBox.config.security.oauth.MemberPrincipal;
 import com.teamY.angryBox.dto.ResponseMessage;
 import com.teamY.angryBox.repository.DiaryRepository;
 import com.teamY.angryBox.repository.FileRepository;
@@ -10,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,6 +31,7 @@ public class DiaryController {
     private final DiaryRepository diaryRepository;
     private final DiaryService diaryService;
     private final FileRepository fileRepository;
+    private final AuthTokenProvider authTokenProvider;
 
     @Transactional
     @PostMapping("diary")
@@ -35,7 +39,10 @@ public class DiaryController {
                                                          @RequestParam int isPublic, @RequestParam int angryFigure,
                                                          @RequestParam int coinBankId, @RequestBody MultipartFile[] file) {
 
-        int memberId = 1;
+        //int memberId = 1;
+        int memberId = ((MemberPrincipal )SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getMemberVO().getId();
+        log.info("memberId : " + memberId);
+//
         DiaryVO diary = new DiaryVO(memberId, title, content, isPublic, angryFigure, coinBankId);
         diaryService.registerDiary(diary, file);
 
