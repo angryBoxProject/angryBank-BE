e00e5a73a36ce5c1dd9d5bc07dc2ec376438ac2c
fidget278 <mills_club@naver.com>
2022-04-24 15:44:49 +0900

    프론트 요청으로 확인용 메시지 추가

--- src/main/java/com/teamY/angryBox/controller/StompController.java
+++ src/main/java/com/teamY/angryBox/controller/StompController.java
@@ -7,14 +7,17 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+import java.util.Map;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class StompController {
     private final SimpMessagingTemplate template; // 특정 broker 로 메세지 전달
 
-    @MessageMapping(value = "topic/bamboo")
-    public void bambooPublish() {
-        template.convertAndSend("/sub/topic/bamboo", "hellooooo");
+    @MessageMapping(value = "topic/bamboo/enter")
+    public void bambooPublish(Map<String, String> data) {
+        log.info(data.get("greeting"));
+        template.convertAndSend("/sub/topic/bamboo", data.get("greeting"));
     }
 }
