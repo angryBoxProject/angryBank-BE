1e13c1fab620978bf5aa805736725926712753ee
da77777 <84134297+da77777@users.noreply.github.com>
2022-04-26 20:28:02 +0900

    diary 목록 조회 시 content 포함(sql문)

--- src/test/java/com/teamY/angryBox/AngryBoxApplicationTests.java
+++ src/test/java/com/teamY/angryBox/AngryBoxApplicationTests.java
@@ -70,7 +70,9 @@ class AngryBoxApplicationTests {
 	@Test
 	void test() {
 
-		log.info("" + diaryRepository.selectDiaryDetail(170).get(0).getFileVO());
+		log.info("" + diaryRepository.selectTodayTop(0, 100));
+
+//		log.info("" + diaryRepository.selectDiaryDetail(170).get(0).getFileVO());
 //		List<DiaryFileVO> df = diaryMapper.selectDiaryDetail(5);
 //		log.info(df.toString());
 		//todackMapper.insertTodack(1, 2, 1);
